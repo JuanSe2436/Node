a9b9e30f0511958e19f59cb9ed55987e9d772e16
Sebas <sebuchas69@gmail.com>
2024-03-28 17:37:00 -0500

    MODULO OS

--- Modulos/App.js
+++ Modulos/App.js
@@ -4,5 +4,21 @@
 
 // console.log(module);
 
-console.log(__filename);
-console.log(__dirname);
+// const path = require('node:path');
+
+// const objPath = path.parse(__filename)
+
+// console.log(objPath.name);
+
+// console.log(__filename);
+// console.log(__dirname);
+
+
+const os = require('os');
+
+var memoriaLibre = os.freemem();
+var memoriaTotal = os.totalmem();
+
+console.log(`Memoria libre: ${memoriaLibre}`)
+console.log(`Memoria total: ${memoriaTotal}`)
+
