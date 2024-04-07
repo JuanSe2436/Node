a54fd239c9563123158c187b68db687eaeb34db4
Sebas <sebuchas69@gmail.com>
2024-04-06 23:05:41 -0500

    PARAMETROS EN LAS RUTAS

--- 05-Express-parte1/app.js
+++ 05-Express-parte1/app.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
 app.get('/api/usuarios', (req, res) => {
     res.send(['sebas', 'luis', 'ana'])
 });
+//SE AGREAGN 2 PUNTOS PARA QUE ENTIENDA QUE ES UN VALOR QUE SE TIENE QUE OBTENER PARA UNA 
+//PARAMETRIZACION
+app.get('/api/usuarios/:year/:mes', (req, res) => {
+    res.send(req.query)
+});
+
 //VARIABLES DE ENTORNO PARA EL PUERTO
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
