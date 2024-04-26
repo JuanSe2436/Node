8e784e7389081f1d2ba74002baa84d7c2b534608
Sebas <sebuchas69@gmail.com>
2024-04-25 21:48:09 -0500

    INSTLACION DE POSTMAN

--- 05-Express-parte1/app.js
+++ 05-Express-parte1/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 //instanciar el elemento
 const app = express();
 
+app.use(express.json());
+
+var usuarios = [
+    { id: 1, nombre: 'Grover' },
+    { id: 2, nombre: 'Sebastian' },
+    { id: 3, nombre: 'Juan' }
+
+];
+
 app.get('/', (req, res) => {
     res.send('HOLA MUNDO DESDE EXPRESS.');
 });  //PETICION
@@ -11,9 +20,25 @@ app.get('/api/usuarios', (req, res) => {
 });
 //SE AGREAGN 2 PUNTOS PARA QUE ENTIENDA QUE ES UN VALOR QUE SE TIENE QUE OBTENER PARA UNA 
 //PARAMETRIZACION
-app.get('/api/usuarios/:year/:mes', (req, res) => {
-    res.send(req.query)
+app.get('/api/usuarios/:id', (req, res) => {
+    let usuario = usuarios.find(u => u.id === parseInt(req.params.id));
+    if (!usuario) res.status(404).send('El usuario no fue encontrado');
+    res.send(usuario);
 });
+
+app.post('/api/usuarios/', (req, res) => {
+    if (!req.body.nombre) {
+        //400 bad request
+        res.status(400)
+    }
+    const usuario = {
+        id: usuarios.length + 1,
+        nombre: req.body.nombre
+    };
+    usuarios.push(usuario);
+    res.send(usuario);
+});
+
 
 //VARIABLES DE ENTORNO PARA EL PUERTO
 const port = process.env.PORT || 3000;
